24a228911f3c1da3848a70d14e2748a2bd55773e
Rezmason <jeremysachs@rezmason.net>
2019-12-04 20:53:41 -0800

    We're up and running with the first program in the renderTextures demo

--- js/renderTextures.js
+++ js/renderTextures.js
@@ -10,50 +10,50 @@ document.body.onload = async () => {
   document.body.appendChild(modicum.canvas);
   await modicum.installTextures();
 
-  const renderTexture = await modicum.makeTexture(1, 1, [], { isFloat: true });
+  // const renderTexture = await modicum.makeTexture(1, 1, [], { isFloat: true });
 
-  // 1. Animate something, render it to a texture
+  const screenQuadIndices = [0, 1, 3, 0, 3, 2];
+  const screenQuadPositions = [-1, -1, -1, 1, 1, -1, 1, 1];
 
-  const program = await modicum.loadProgram(
-    "shaders/textured.vert",
-    "shaders/textured.frag"
+  const program1 = modicum.makeProgram(
+    `
+    attribute vec2 aPos;
+    varying vec2 vUV;
+    void main(void) {
+      vUV = (aPos + 1.) * 0.5;
+      gl_Position = vec4(aPos, 0.0, 1.0);
+    }
+  `,
+    `
+    precision mediump float;
+    varying vec2 vUV;
+    uniform float uTime;
+    void main(void) {
+      gl_FragColor = vec4(abs(fract(vUV + uTime) - 0.5) * 2., 0., 1.0);
+    }
+  `
   );
-  const scene = program.makeUniformGroup();
-  const camera = mat4.create();
-  const transform = mat4.create();
-  scene.setUniforms({
-    uSampler: await modicum.loadImageTexture("assets/crate.bmp"),
-    uTransform: transform
-  });
 
-  const mesh = program
+  const mesh1 = program1
     .makeMesh(4, 2)
-    .setVertex(0, {
-      aPos: [-1, -1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-      aUV: [0, 0, 0, 1, 1, 0, 1, 1]
-    })
-    .setIndex(0, [0, 1, 3, 0, 3, 2])
-    .update();
-
-  program.activate();
+    .setVertex(0, { aPos: screenQuadPositions })
+    .setIndex(0, screenQuadIndices)
+    .update()
+    .setUniforms({ uTime: [0] });
 
   const { resize } = makeResizer(modicum, (width, height) => {
-    const aspectRatio = width / height;
-    mat4.perspective(camera, (Math.PI / 180) * 90, aspectRatio, 0.0001, 1000);
-    mat4.translate(camera, camera, vec3.set(vec3.create(), 0, 0, -1));
-    scene.setUniforms({ uCamera: camera });
     redraw();
   });
 
   const animator = makeAnimator((time, delta) => {
-    mat4.rotateZ(transform, transform, delta);
-    scene.setUniforms({ uTransform: transform });
+    mesh1.setUniforms({ uTime: [time] });
     redraw();
   });
 
   const redraw = () => {
-    modicum.clear(null, true);
-    program.drawMesh(mesh, scene);
+    modicum.clear(null);
+    program1.activate();
+    program1.drawMesh(mesh1);
   };
 
   window.onresize = resize;
